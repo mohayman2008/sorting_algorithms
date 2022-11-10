acd2aa33310326cc0e0fb5738a417806a097bfae
Mohamed Ayman <mohayman2007@gmail.com>
2022-11-10 07:21:40 +0000

    Task 11 - "Quick Sort - Hoare Partition scheme"

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -28,7 +28,7 @@ void swap(int *n1, int *n2)
  */
 void quick_sort_hoare_r(int *arr, size_t len, int *array, size_t size)
 {
-	size_t itr1, itr2, idx;
+	size_t itr1, itr2;/*, idx;*/
 	int pivot;
 
 	if (!array || size < 2)
@@ -38,26 +38,24 @@ void quick_sort_hoare_r(int *arr, size_t len, int *array, size_t size)
 	itr1 = 0;
 	itr2 = size - 1;
 
-	while (itr2 > itr1)
+	while (1)
 	{
-		while (array(itr1) < pivot)
+		while (array[itr1] < pivot)
 			itr1++;
-		while (array(itr2) > pivot)
+		while (array[itr2] > pivot)
 			itr2--;
 		if (itr1 >= itr2)
-		{
-			if (array[itr1] == pivot)
-				idx = itr1;
-			else
-				idx = itr2;
 			break;
-		}
 		swap(array + itr1, array + itr2);
+		itr1++;
+		itr2--;
 		print_array(arr, len);
 	}
 
-	quick_sort_hoare_r(arr, len, array, p_idx + 1);
-	quick_sort_hoare_r(arr, len, array + p_idx + 1, size - p_idx - 1);
+	if (itr2 == size - 1) /* The pivot sorted position is the last spot */
+		itr2--;
+	quick_sort_hoare_r(arr, len, array, itr2 + 1);
+	quick_sort_hoare_r(arr, len, array + itr2 + 1, size - itr2 - 1);
 }
 
 /**
@@ -69,7 +67,7 @@ void quick_sort_hoare_r(int *arr, size_t len, int *array, size_t size)
  *
  * Description: wrapper function for quick_sort_recursive()
  */
-void quick_sort_hoare(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size)
 {
 	quick_sort_hoare_r(array, size, array, size);
 }
